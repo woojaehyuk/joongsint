5dd51106a8cd1428971fcaa8c061e5f0dbb26cfa
exit1100 <85146195+exit1100@users.noreply.github.com>
2023-03-24 01:06:02 +0900

    pm2 start js file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'jungsint',
+      script: './node_modules/@vue/cli-service/bin/vue-cli-service.js',
+      args: 'serve --port 3000',
+      instances: 0,
+      exec_mode: 'fork',
+    },
+  ],
+};
