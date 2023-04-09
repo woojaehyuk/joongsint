9a07c5ab50542cb80773b2c990cff66ec6206742
exit1100 <85146195+exit1100@users.noreply.github.com>
2023-04-09 23:31:20 +0900

    UPDATE: 기업 소개 수정

--- src/components/Board/IntroComponent.vue
+++ src/components/Board/IntroComponent.vue
@@ -8,11 +8,11 @@
             <h1>기업 소개</h1>
             <br />
             <div>
-              JungSint는 2023년, OSINT(공개 출처 정보) 솔루션을 제공하기 위해
+              JoongSint는 2023년, OSINT(공개 출처 정보) 솔루션을 제공하기 위해
               중부대학교 SCP팀이 만든 가상 모의 기업이다. 이러한 공개된 정보를
               바탕으로 이를 수집, 가공, 응용하여 웹 상에 돌아다니는 기업 또는
               사람에 대한 위험한 정보들을 조회하고 이를 예방할 수 있도록 해준다.
-              Jungsint는 이러한 기능들을 사용자에게 제공하기 위해 웹 개발과
+              Joongsint는 이러한 기능들을 사용자에게 제공하기 위해 웹 개발과
               다양한 툴을 자동화해주는 모듈을 제작하여 배포한다.
             </div>
             <br /><br />
@@ -84,7 +84,8 @@
                     :src="require('@/assets/member3.png')"
                   ></b-card-img>
                   <b-card-text><b>우제혁</b> (총괄 및 운영)</b-card-text>
-                  <b-card-text> wpgur@gmail.com </b-card-text>
+                  <b-card-text> 직급 : CEO </b-card-text>
+                  <b-card-text> 이메일 : wpgur0510@gmail.com </b-card-text>
                 </b-card>
               </v-col>
 
@@ -102,7 +103,8 @@
                   ></b-card-img
                   ><br />
                   <b-card-text><b>송태현</b> (서비스 개발) </b-card-text>
-                  <b-card-text> 정보1 </b-card-text>
+                  <b-card-text> 직급 : 검색부장 </b-card-text>
+                  <b-card-text> 이메일 : </b-card-text>
                 </b-card>
               </v-col>
 
@@ -119,6 +121,7 @@
                     :src="require('@/assets/member2.png')"
                   ></b-card-img>
                   <b-card-text><b>신재형</b> (서비스 개발)</b-card-text>
+                  <b-card-text> 직급 : 네트워크 부장 </b-card-text>
                   <b-card-text> 이메일 : shinwogud12@naver.com </b-card-text>
                 </b-card>
               </v-col>
@@ -136,6 +139,7 @@
                     :src="require('@/assets/member4.jpg')"
                   ></b-card-img>
                   <b-card-text><b>이예준</b> (서비스 개발)</b-card-text>
+                  <b-card-text> 직급 : 크롤링 부장 </b-card-text>
                   <b-card-text> 이메일 : exit3015@gmail.com </b-card-text>
                 </b-card>
               </v-col>
@@ -153,7 +157,8 @@
                     :src="require('@/assets/member5.png')"
                   ></b-card-img>
                   <b-card-text><b>이정호</b> (서비스 개발)</b-card-text>
-                  <b-card-text> wjdgh7575@naver.com </b-card-text>
+                  <b-card-text> 직급 : 서버 부장 </b-card-text>
+                  <b-card-text> 이메일 : wjdgh7575@naver.com </b-card-text>
                 </b-card>
               </v-col>
             </v-row>
